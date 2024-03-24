5d79d4466f0a649c007649d056e587a7a0353a4e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-24 14:11:12 +0200

    Profile PHP

---
+++ backend/profile.php
@@ -0,0 +1,28 @@
+<?php
+
+include './connection.php';
+
+$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
+$image = isset($_POST['image']) ? $_POST['image'] : '';
+$bio = isset($_POST['bio']) ? $_POST['bio'] : '';
+$experience = isset($_POST['experience']) ? $_POST['experience'] : '';
+$education = isset($_POST['education']) ? $_POST['education'] : '';
+$skills = isset($_POST['skills']) ? $_POST['skills'] : '';
+
+$query = $mysqli->prepare('INSERT INTO profiles (user_id, image, bio, experience, education, skills) VALUES (?,?,?,?,?,?)');
+
+$query->bind_param('isssss', $user_id, $image, $bio, $experience, $education, $skills);
+
+$response = [];
+
+if ($query->execute()) {
+    $response['success'] = true;
+    $response['message'] = "Profile added";
+} else {
+    $response['success'] = false;
+    $response['message'] = "Error, Profile cannot added";
+}
+
+echo json_encode($response);
+
+$mysqli->close();
