007742d306c99977a059f955dfe094f5507e7ac0
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-26 01:08:16 +0200

    Editing display post

--- frontend/src/pages/Home/components/Posts.jsx
+++ frontend/src/pages/Home/components/Posts.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import profile from '../../Assets/profile.jpg';
+import sheet from '../../Assets/cheat_sheet.jpeg';
 
 const Posts = () => {
   return (
     <div className="post-box">
-      <div className="post_profile">
-        <img src={profile} alt="profile" />
-      </div>
       <div className='info'>
-        <div className='name'>
-          hussein
+        <div className="post_profile">
+          <img src={profile} alt="profile" />
         </div>
-        <div className='position'>
-          web dev
+        <div>
+          <div className='name'>
+            hussein abou zeinab
+          </div>
+          <div className='position'>
+            web developer
+          </div>
         </div>
       </div>
       <div className='description'>
-        <div className="post">
-          asdasdasdasdasdasds
+        <div className="content">
+          I've received many 6-figure job offers because I was the most prepared for the job interview, not because I was the "smartest".
+        </div>
+        <div className='image'>
+          <img src={sheet} alt="profile" />
         </div>
       </div>
     </div>
